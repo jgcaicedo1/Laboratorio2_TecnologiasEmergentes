1855ef3c99946ae7aad567aa1c5d834aeaa5e9a4
jgcaicedo1 <62907581+jgcaicedo1@users.noreply.github.com>
2024-06-04 18:20:02 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,11 @@ export default function Home({ formattedDate }) {
   return (
     <>
       <h1>Laboratorio 2</h1>
-      <p>Fecha {formattedDate}.</p>
+      <p>Fecha 04-06-2024.</p>
       <ul>
         <li>Gabriel Caicedo</li>
         <li>Christian Ortiz</li>
       </ul>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const buildDate = Date.now();
-  const formattedDate = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "long",
-    timeStyle: "long",
-  }).format(buildDate);
-
-  return { props: { formattedDate } };
 }
